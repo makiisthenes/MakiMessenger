fb575b746745df9a4e4732bac06cbb9b5f285f59
makiisthenes <52138450+makiisthenes@users.noreply.github.com>
2020-06-03 18:44:09 +0100

    Add files via upload

---
+++ Security/Email Validation.py
@@ -0,0 +1,28 @@
+# from validate_email import validate_email  - ommited
+from email_validator import validate_email, EmailNotValidError
+
+'''
+def email_val(user):
+    print("Please wait while we verify your email...")
+    is_valid = validate_email(user, verify=True)
+    if is_valid:
+        verify_user(user)
+        return True
+    else:
+        return False
+
+email_val("michaelperes1@gmail.com")
+'''
+
+
+def email_val(user):
+    try:
+        print("Please wait while we validate email...")
+        valid = validate_email(user)
+        email = valid.email
+        print("This email is valid :)")
+        return True
+    except EmailNotValidError as e:
+        print(str(e))
+        return e
+
