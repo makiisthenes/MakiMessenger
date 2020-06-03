3f294d3adc658ac5132d31391972804b5f0ef367
makiisthenes <52138450+makiisthenes@users.noreply.github.com>
2020-06-03 18:36:26 +0100

    Add files via upload

--- Server_REWORK/email_valid.py
+++ Server_REWORK/email_valid.py
@@ -1,10 +1,30 @@
-from validate_email import validate_email
+# from validate_email import validate_email  - ommited
+from email_validator import validate_email, EmailNotValidError
 from twofactor import verify_user
 
+'''
 def email_val(user):
+    print("Please wait while we verify your email...")
     is_valid = validate_email(user, verify=True)
     if is_valid:
         verify_user(user)
         return True
     else:
         return False
+
+email_val("michaelperes1@gmail.com")
+'''
+
+
+def email_val(user):
+    try:
+        print("Please wait while we validate email...")
+        valid = validate_email(user)
+        email = valid.email
+        verify_user(user)
+        print("This email is valid :)")
+        return True
+    except EmailNotValidError as e:
+        print(str(e))
+        return e
+
