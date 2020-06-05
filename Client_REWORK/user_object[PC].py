7d136f6d1109fab5c2ce75cc8c6710269cda4462
makiisthenes <52138450+makiisthenes@users.noreply.github.com>
2020-06-05 22:43:51 +0100

    Add files via upload

--- Client_REWORK/user_object[PC].py
+++ Client_REWORK/user_object[PC].py
@@ -1,5 +1,19 @@
 # This will be used to collect information about the user and validate along side credentials.
 
+class UserObject:
+    def __init__(self, timestamp, priv_ip, mac, hostname, system, release, version, pub_ip):
+        self.timestamp = timestamp
+        self.priv_ip = priv_ip
+        self.mac = mac
+        self.hostname = hostname
+        self.system = system
+        self.release = release
+        self.version = version
+        self.pub_ip = pub_ip
+    def parser(self, text_file):
+        # discontinued currently.
+        pass
+
 
 # Time Created
 import time
@@ -20,12 +34,13 @@ def get_ip():
     finally:
         s.close()
     return IP
-IP = get_ip()
+priv_ip = get_ip()
 print(f"Private IP: {get_ip()}")
 
 
 # MAC address of Client.
 import netifaces  # Cross Platform :)
+mac = ''
 for x in netifaces.interfaces():
     if len(netifaces.ifaddresses(x)) > 1:
         # print(netifaces.ifaddresses(x))
@@ -34,7 +49,7 @@ for x in netifaces.interfaces():
             item = netifaces.ifaddresses(x)[dict][0]['addr']
             pair.append(item)
         # print(pair)
-        if pair[1] == IP:
+        if pair[1] == priv_ip:
             mac = pair[0]
             print(f"MAC: {mac}")
         # Check the list for this ip to crosscheck mac address.
@@ -48,11 +63,13 @@ print("Cant find useful screensize module for pc, smartphone compatible.")
 
 # Device Type/ OS
 import socket
-hostname = socket.gethostname()
+hostname0 = socket.gethostname()
 # print(hostname)
 # Option 2
 import platform
-print(f"HostName: {platform.node()}")
+hostname = platform.node()
+print(f"HostName: {hostname}")
+
 # OS
 system = platform.system()
 release = platform.release()
@@ -65,3 +82,6 @@ from requests import get
 pub_ip = get('https://api.ipify.org').text
 print(f"Public IP: {pub_ip}")
 
+
+user = UserObject(timestamp, priv_ip, mac, hostname, system, release, version, pub_ip)
+print("User Object has been created and can be pickled now")
