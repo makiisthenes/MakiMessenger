ceb9e36a63bfc1c8ecceaaab450195081a80d33f
makiisthenes <52138450+makiisthenes@users.noreply.github.com>
2020-06-10 20:02:35 +0100

    Update noob_script.py

--- noob_script.py
+++ noob_script.py
@@ -3,6 +3,12 @@ path = os.getcwd()
 client_path = os.path.join(path, 'Client_REWORK')
 server_path = os.path.join(path, 'Server_REWORK')
 
+with open("im_a_noob_help_click_me.cmd", "w+") as noob:
+    noob.write(f"cd {path} \n")
+    noob.write(f"pip install -r requirements.txt")
+subprocess.call("im_a_noob_help_click_me.cmd")
+with open("im_a_noob_help_click_me.cmd", "w+") as noob:
+    noob.write("python noob_script.py)
 print(path)
 with open("server_click_here.cmd", 'w+') as noob:
     noob.write(f"cd {server_path} \n")
