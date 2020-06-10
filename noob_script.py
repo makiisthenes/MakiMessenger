0618910a1f04c78fa4ac4d1babdac6b12ec2f188
makiisthenes <52138450+makiisthenes@users.noreply.github.com>
2020-06-10 19:53:32 +0100

    Update noob_script.py

--- noob_script.py
+++ noob_script.py
@@ -7,7 +7,7 @@ print(path)
 with open("im_a_noob_help.cmd", 'w+') as noob:
     noob.write(f"cd {path} \n")
     noob.write("pip install -r requirements.txt \n")
-    noob.write("python noob_script.py")
+    noob.write("python noob_script.py \n")
 with open("server_click_here.cmd", 'w+') as noob:
     noob.write(f"cd {server_path} \n")
     noob.write(f"python server.py \n")
